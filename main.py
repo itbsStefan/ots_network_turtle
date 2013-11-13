e3d4dc8fcbc3420a16fbcff507f7be02bc8457f9
Stefan Gehrmann <itbs@visproxy.net>
2013-11-14 00:23:00 +0100

    Codeteile in Oturtle Klasse verlagert

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
 # coding=utf-8
+import helper as h
 import turtle
 from otsTurtleClasses import Oturtle
 
-def show(dictionary):
-  print "=========================="
-  print "=  Turtle Configuration  ="
-  print "=========================="
-  for key in sorted(dictionary.keys()):
-    print key, ":", dictionary[key]
-  print "=========================="
-  print
 
-def callback(e):
-	print e.keysym, repr(e.char)
 
 w=turtle.Screen()
-#t = turtle.getturtle()
+t = turtle.getturtle()
+cv = turtle.getcanvas()
 
 length = 10.0
 turtle.register_shape("dreieck", ((5,-3),(0,5),(-5,-3)))
-
+print w.getshapes()
 ot = Oturtle("dreieck","dreieck")
 #ot.showturtle()
 ot.printName()
@@ -27,29 +19,19 @@ ot.printName()
 
 
 
-t1 = turtle.Turtle()
-t1.shape("dreieck")
-t1.color("blue","purple")
-t1.resizemode("auto")
-t1.pensize(2)
-t1.speed(5)
-t1.up()
-t1.setheading(180)
-t1.goto(50, 0)
-t1.down()
 
-t2 = turtle.Turtle()
-t2.shape("turtle")
-t2.color("red","yellow")
-t2.resizemode("auto")
-t2.speed(3)
-t2.up()
-t2.setheading(0)
-t2.goto(-50, 0)
-t2.down()
+t1 = Oturtle("t1", "turtle", True, ("blue","purple"))
+t1.setup(50,None, head = 180, speed=5, size = 2)
+
+t2 = Oturtle("t2", "turtle", True, ("red","yellow"))
+t2.setup(-50, None, 0, 3, 3)
 
 
-
+for s in w.turtles():
+  name = None
+  if type(s).__name__ == "Oturtle":
+    name = s.name
+  print s.__class__, name
 
 
 
@@ -64,8 +46,7 @@ def togglePen():
 		t2.pendown()
 	print "Penstatus t2",t2.pen()["pendown"]
 
-
-
+w.onkey(togglePen, "p")
 
 
 def moveTurtle(length, direction):
@@ -85,11 +66,11 @@ def turtleRight():
 def turtleDown():
 	moveTurtle(length, 270)
 
-turtle.onkey(turtleUp, "Up")  #  90
-turtle.onkey(turtleDown, "Down")# 270
-turtle.onkey(turtleLeft, "Left")# 180
-turtle.onkey(turtleRight, "Right")#  0
-turtle.onkey(togglePen, "p")
+w.onkey(turtleUp, "Up")  #  90
+w.onkey(turtleDown, "Down")# 270
+w.onkey(turtleLeft, "Left")# 180
+w.onkey(turtleRight, "Right")#  0
+
 
 
 def moveT2(length, direction):
@@ -109,10 +90,10 @@ def turtle2Right():
 def turtle2Down():
 	moveT2(length, 270)
 
-turtle.onkey(turtle2Up, "w")  #  90
-turtle.onkey(turtle2Down, "s")# 270
-turtle.onkey(turtle2Left, "a")# 180
-turtle.onkey(turtle2Right, "d")#  0
+w.onkey(turtle2Up, "w")  #  90
+w.onkey(turtle2Down, "s")# 270
+w.onkey(turtle2Left, "a")# 180
+w.onkey(turtle2Right, "d")#  0
 
 
 
@@ -123,18 +104,69 @@ def dot():
   t1.dot()
   t2.dot()
 turtle.onkey(dot, "o")
-turtle.onkey(stamp, "i")
+turtle.onkey(stamp, "space")
 
 
-show(turtle._CFG)
-print "use curserkeys to move turtle1 and  w|a|s|d for tutle2 (purple)"
-
-turtle.listen()
-turtle.exitonclick()
+h.show(turtle._CFG)
+print "use curserkeys to move turtle1 and  w|a|s|d for tutle2 (purple) and x for close the Window"
 
 
+def testfun():
+  x = t2.pen()["pendown"]
+  t2.stamp()
+  t2.pendown()
+  t2.circle(100)
+  t2.pen()["pendown"] = x
+
+t2.onkey(testfun,"j")
+testfun()
+
+#import Tkinter
+#r = Tkinter.Tk()
+#r.bind("<KeyPress>", h.callback)
+
+w.listen()
+w.onclick(t.goto)
+#turtle.exitonclick()
+w.onkey(w.bye, "x")
 
 
 
 
+
+#import threading, time
+#import Tkinter
+
+#root = Tkinter.Tk()
+##root = turtle.getcanvas()
+#def ping():
+#  while 1:
+#    time.sleep(1)
+#    print root.bind()
+#    root.event_generate('<<Ping>>', when='tail')
+#    print "it worked"
+
+
+#v = Tkinter.BooleanVar()
+#v.set(0)
+#def gotPing(event):
+#  v.set(not v.get())
+
+#check = Tkinter.Checkbutton(root, variable=v, text='Ping!')
+#check.pack(side=Tkinter.TOP)
+#butto = Tkinter.Button(root, text='Quit', command=root.quit)
+#butto.pack(side=Tkinter.TOP)
+#s = "Test"
+#l = Tkinter.Label(root,text=s)
+#l.pack(side=Tkinter.TOP)
+
+#root.bind('<<Ping>>', gotPing)
+#th = threading.Thread(target=ping)
+#th.setDaemon(1)
+#th.start()
+
+#root.mainloop()
+
+turtle.mainloop()
+#w.mainloop()
 
--- otsTurtleClasses.py
+++ otsTurtleClasses.py
@@ -1,23 +1,81 @@
 # coding=utf-8
 from turtle import Turtle
-from random import choice
-from string import lowercase
+from random import choice    # Choose a random element from a non-empty sequence
+from random import randint   # Return random integer in range [a, b], including both end points
+from string import lowercase # Rerurns 'abcdefghijklmnopqrstuvwxyz'
+
 
 class Oturtle(Turtle):
+
+  name = None
+  cv = None
+  _keys = None
 
   def randomString(self, length = 5):
     return "".join(choice(lowercase) for i in range(length))
 
   def __init__(self,name = "", shape = "turtle", visible = False, colors = None):
     Turtle.__init__(self, shape, visible=visible)
+    self.cv = Turtle._screen._canvas
     if name =="":
       self.name = self.randomString(8)
     else:
       self.name = name
     if colors == None:
       self.color("black", "pink")
+    else:
+      self.color(colors[0],colors[1])
+    self._keys = []
 
   def printName(self):
     print self.__class__.__name__, self.name
+
+  def setup(self, x = None, y = None, head = None, speed=None, size = 1):
+    self.resizemode("auto")
+    self.pensize(size)
+    if speed == None:
+      self.speed(randint(1,10))
+    else:
+      self.speed(speed)
+      if speed < 0 or speed > 10:
+        print "Speed has bad value set to 0 by", self.name, self
+        self.speed(0)
+    if head == None:
+      self.setheading(randint(0,359.9))
+    else:
+      self.setheading(head)
+    s = self.getscreen()
+    h = (s.window_height()-10)/2
+    w = (s.window_width()-10)/2
+    if x == None:
+      x = randint(-w,w)
+    if y == None:
+      y = randint(-h,h)
+    self.up()
+    self.goto(x,y)
+    self.down()
+
+  def _onkey(self, fun, key):
+    """Bind fun to key-release event of key.
+    Canvas must have focus. See method listen
+    """
+    if fun is None:
+      self.cv.unbind("<KeyRelease-%s>" % key, None)
+    else:
+      def eventfun(event):
+        fun()
+      self.cv.bind("<KeyRelease-%s>" % key, eventfun)
+
+  def onkey(self, fun, key):
+    """Bind fun to key-release event of key.
+    """
+    if fun == None:
+      if key in self._keys:
+        self._keys.remove(key)
+      elif key not in self._keys:
+        self._keys.append(key)
+      self._onkey(fun, key)
+
+
 
 
