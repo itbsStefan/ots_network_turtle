f4c8cccf933c22f63e3798b2a4f371755857da7a
Stefan Gehrmann <itbs@visproxy.net>
2013-11-14 03:58:25 +0100

    Verwendung oTurtle und task-Event in Class

--- timed_turtle.py
+++ timed_turtle.py
@@ -1,17 +1,27 @@
-import turtle
+# -*- coding: utf-8 -*-
+from turtle import Screen
+from otsTurtleClasses import Oturtle
 
-turtle.forward(1)
-root = turtle.Screen()._root
+
+t = Oturtle("Turtle", "turtle", True, ("blue","red"))
+t.setup(50,None, head = 180, speed=5, size = 2)
+
+root = Screen()._root
 
 fwd = 1
-
-
 def task():
     global fwd
-    turtle.forward(fwd)
-    turtle.right(33)
+    t.forward(fwd)
+    t.right(33)
     fwd += 1
     root.after(1, task)  # reschedule event in 1 milli second
+
+
+Screen().onclick(t.goto)
+Screen().onkey(Screen().bye, "x")
+root.bind("g", t.gruen)
+root.bind("e", t.eventCatcher)
+Screen().listen()
 
 root.after(1, task)
 root.mainloop()
